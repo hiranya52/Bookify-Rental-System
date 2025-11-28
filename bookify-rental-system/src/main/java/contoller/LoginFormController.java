5c588c3458144caf14a75a9b8bf494a88a3a7508
Hiranya Mendis <hiranyamendis52@gmail.com>
2025-11-28 23:29:15 +0530

    Design login page

--- bookify-rental-system/src/main/java/contoller/LoginFormController.java
+++ bookify-rental-system/src/main/java/contoller/LoginFormController.java
@@ -1,4 +1,29 @@
 package contoller;
 
-public class LoginFormController {
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginFormController implements Initializable {
+
+    @FXML
+    private JFXComboBox<?> cmbRole;
+
+    @FXML
+    private JFXPasswordField txtPassword;
+
+    @FXML
+    private JFXTextField txtUsername;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+
+    }
 }
