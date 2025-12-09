1fb589f3cc111fa3d793ab2cc5939417a237db7f
Hiranya Mendis <hiranyamendis52@gmail.com>
2025-12-09 22:12:48 +0530

    Add Customer DTO class

--- bookify-rental-system/src/main/java/contoller/AdminDashboardController.java
+++ bookify-rental-system/src/main/java/contoller/AdminDashboardController.java
@@ -58,7 +58,16 @@ public class AdminDashboardController {
 
     @FXML
     void btnLogOutOnAction(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/login_form.fxml"));
+            Scene scene = new Scene(loader.load());
 
+            Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+
+            stage.setScene(scene);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
---
+++ bookify-rental-system/src/main/java/model/dto/CustomerDTO.java
@@ -0,0 +1,19 @@
+package model.dto;
+
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class CustomerDTO {
+
+    private String Id;
+    private String date;
+    private String name;
+    private String phoneNo;
+    private String email;
+
+}
