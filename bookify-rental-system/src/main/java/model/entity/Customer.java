f493fc64311c1c4a81b5941e08d6412b67bcbb1e
Hiranya Mendis <hiranyamendis52@gmail.com>
2025-12-09 22:18:59 +0530

    Add Customer Entity class

---
+++ bookify-rental-system/src/main/java/model/entity/Customer.java
@@ -0,0 +1,18 @@
+package model.entity;
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Customer {
+
+    private String Id;
+    private String date;
+    private String name;
+    private String phoneNo;
+    private String email;
+
+}
