23a44df873c85050b000c28f25b335edbbcdff99
Hiranya Mendis <hiranyamendis52@gmail.com>
2025-12-21 19:53:48 +0530

    Design User Management UI

---
+++ bookify-rental-system/src/main/java/contoller/UserInfoController.java
@@ -0,0 +1,76 @@
+package contoller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class UserInfoController {
+
+    @FXML
+    private Label lblAdministrator;
+
+    @FXML
+    private Label lblBookID;
+
+    @FXML
+    private Label lblDate;
+
+    @FXML
+    private JFXTextField txtAuthor;
+
+    @FXML
+    private JFXTextField txtCategory;
+
+    @FXML
+    private JFXTextField txtCategory1;
+
+    @FXML
+    private JFXTextField txtTitle;
+
+    @FXML
+    void btnAddBookOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnBooksOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCustomersOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDashboardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLogOutOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRentalsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReportsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReturnsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUserManagementOnAction(ActionEvent event) {
+
+    }
+
+}
