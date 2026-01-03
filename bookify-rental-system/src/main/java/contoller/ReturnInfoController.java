34cee1ae301a6916ac95b6b6b5e6034e57fde2ef
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-01-03 19:07:56 +0530

    Design Returns Info UI

---
+++ bookify-rental-system/src/main/java/contoller/ReturnInfoController.java
@@ -0,0 +1,102 @@
+package contoller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class ReturnInfoController {
+
+    @FXML
+    private TableColumn<?, ?> colBookID;
+
+    @FXML
+    private TableColumn<?, ?> colCustomer;
+
+    @FXML
+    private TableColumn<?, ?> colDueDate;
+
+    @FXML
+    private TableColumn<?, ?> colFine;
+
+    @FXML
+    private TableColumn<?, ?> colIssueDate;
+
+    @FXML
+    private TableColumn<?, ?> colOverDueDate;
+
+    @FXML
+    private TableColumn<?, ?> colRentalID;
+
+    @FXML
+    private Label lblAdministrator;
+
+    @FXML
+    private Label lblCusID;
+
+    @FXML
+    private Label lblDate;
+
+    @FXML
+    private Label lblRentID;
+
+    @FXML
+    private TableView<?> tblRentDetails;
+
+    @FXML
+    private JFXTextField txtBookId;
+
+    @FXML
+    private JFXTextField txtCustomer;
+
+    @FXML
+    private JFXTextField txtFine;
+
+    @FXML
+    void btnBooksOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCompleteRentalOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCustomersOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDashboardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLogOutOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRentalsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReportsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReturnsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUserManagementOnAction(ActionEvent event) {
+
+    }
+
+}
